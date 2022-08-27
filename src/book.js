356d286518384e49973b654f0d22fddecea65e60
Angie Staffieri <angie.staffieri@gmail.com>
2022-08-27 10:33:16 -0600

    Initial Commit

--- src/book.js
+++ src/book.js
@@ -1,11 +1,28 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
 }
+
+ function buildMainCharacter(name, age, pronouns) {
+   var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+    }
+    return character
+  }
+
+  function saveReview (comment, reviews) {
+    if (!reviews.includes(comment)) {
+      return reviews.push(comment)
+    } else {
+      return
+    }
+  }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
