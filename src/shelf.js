59a37c58c9194c7dcdccae0e81f460eb7c397a16
Angie Staffieri <angie.staffieri@gmail.com>
2022-08-28 18:59:46 -0600

    More things are done

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,19 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    return shelf.unshift(book)
+    }
+  }
+
+function unshelfBook(book, shelf) {
+  for (var i=0; i < shelf.length; i++) {
+    return shelf.splice(book)
+    }
+  }
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
